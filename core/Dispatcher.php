0fc1738399784fd9ad4558404dec0cb246b1ce01
Youssef KAAOUACHI <ykaaouachi@gmail.com>
2016-11-15 10:38:59 +0000

    Edit dispatcher

--- core/Dispatcher.php
+++ core/Dispatcher.php
@@ -10,7 +10,8 @@ class Dispatcher {
         $controller = $this->loadController();
         
         if(!in_array($this->request->action, get_class_methods($controller))){
-            $this->error('Le controller ' . $this->request->controller . ' n\'a pas de méthode : ' . $this->request->action);
+            //$this->error('Le controller ' . $this->request->controller . ' n\'a pas de méthode : ' . $this->request->action);
+            $this->error('Action introuvable');
         }
         call_user_func_array(array($controller, $this->request->action), $this->request->params);
         $controller->render($this->request->action);
@@ -24,7 +25,9 @@ class Dispatcher {
     function error($msg){
         header("HTTP/1.0 404 Not found");
         $controller = new Controller($this->request);
+        //Add message to view
         $controller->set(array("message" => $msg));
+        // render view 404
         $controller->render('/errors/404');
         die();
     }
